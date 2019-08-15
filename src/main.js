974967d04b7e30c905bb28e60f0bcce10043fdb1
Peter Pham <phampp@amazon.com>
2019-08-15 10:03:14 +1000

    add Amplify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-console */
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false
+
+console.log(Amplify)
 
 new Vue({
   render: h => h(App),
